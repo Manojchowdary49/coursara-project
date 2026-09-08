510aeb46b25c13f697b1d00111dc983e9d1137b4
Manojchowdary49 <gorantlamanoj727@gmail.com>
2026-09-08 20:51:13 +0530

    Update ESLint configuration for JavaScript files
    Add file pattern for JavaScript files and define globals.

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,14 @@ const eslint = require('@eslint/js');
 module.exports = [
   eslint.configs.recommended,
   {
+    files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: 'commonjs'
+      sourceType: 'commonjs',
+      globals: {
+        test: 'readonly',
+        expect: 'readonly'
+      }
     },
     rules: {
       semi: ['error', 'always'],
